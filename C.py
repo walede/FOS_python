8774061e4df8bc796a837403b3c0c33d91a42ce2
Rafeeq Durowoju <59541303+walede@users.noreply.github.com>
2020-01-23 20:23:02 -0500

    Implemented Numpy
    Implemented numpy across the project to decrease computational time.

--- C.py
+++ C.py
@@ -1,13 +1,14 @@
-import statistics as stats
+import numpy as np
 from D import D_Array
-import copy
+
+
 class C_Array(D_Array):
-    def __init__(self,N_o, size=10):
+    def __init__(self, N_o, size=10):
         super().__init__(N_o, size=10)
-        self.C=[0]*size
-        self.C_temp=[0]*size
-        self.C_reign=[0]*size
-        self.No=N_o
+        self.C = np.zeros((size))
+        self.C_temp = np.zeros((size))
+        self.C_reign = np.zeros((size))
+        self.No = N_o
 
     def C_func(self,y_out,m,pot_can):
         """
@@ -18,18 +19,14 @@ class C_Array(D_Array):
             return self.C[m]
         #if m is not equal to 0
         elif m:
-            r_sum=0
+            r_sum = 0
             for r in range(m): 
-                r_sum += self.alpha_func(pot_can,m,r)*self.C_func(y_out,r,pot_can)
-            j=self.No
-            result=[]
-            for i in y_out[self.No::]:
-                result.append(i*pot_can[j])
-                j+=1
-            self.C[m]=stats.mean(result) - r_sum
+                r_sum += self.alpha_func(pot_can,m,r) * self.C_func(y_out,r,pot_can)
+            result = np.multiply(pot_can[self.No::], y_out[self.No::])
+            self.C[m] = np.mean(result) - r_sum
             return self.C[m] 
         else:
-            self.C[m]=stats.mean(y_out[self.No::])
+            self.C[m] = np.mean(y_out[self.No::])
             return self.C[m]        
             
     def temp_to_def(self):
@@ -37,7 +34,7 @@ class C_Array(D_Array):
         Copies the values in the temporary C list to the 
         default list.
         """
-        self.C=copy.deepcopy(self.C_temp)
+        self.C = np.copy(self.C_temp)
         return None
 
     def reign_to_temp(self):
@@ -45,7 +42,7 @@ class C_Array(D_Array):
         Copies the values in the reigning C list 
         to the temporary list.
         """
-        self.C_temp=copy.deepcopy(self.C_reign)
+        self.C_temp = np.copy(self.C_reign)
         return None
     
     def def_to_reign(self):
@@ -53,5 +50,5 @@ class C_Array(D_Array):
         Copies the values in the default C list 
         to the reigning list.
         """
-        self.C_reign=copy.deepcopy(self.C)
+        self.C_reign = np.copy(self.C)
         return None
